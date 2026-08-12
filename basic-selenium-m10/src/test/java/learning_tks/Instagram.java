27bef2c2639793c818f295b2fc75acf0f8e78ac3
AutomationWithPiyushhh <vasudevahir21@gmail.com>
2026-08-12 10:29:16 +0530

    ss of webelement

--- basic-selenium-m10/src/test/java/learning_tks/Instagram.java
+++ basic-selenium-m10/src/test/java/learning_tks/Instagram.java
@@ -2,10 +2,12 @@ package learning_tks;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.io.FileHandler;
 
@@ -13,19 +15,31 @@ public class Instagram {
 	public static void main(String[] args) throws InterruptedException, IOException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		
-		driver.get("https://www.instagram.com/");
-		
-		TakesScreenshot tks = (TakesScreenshot) driver;
-		
-		File src = tks.getScreenshotAs(OutputType.FILE);
-		
-//		get the java representation object of the physical file for destination
-		File dest = new File("./errorshots/insta.png");
-//		File dest = new File("C:/Users/User/git/basic-sel-m10/basic-selenium-m10/errorshots/insta.png");
-		
-		FileHandler.copy(src, dest);
-		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+		driver.get("https://www.facebook.com/");
+
+//		TakesScreenshot tks = (TakesScreenshot) driver;
+//		this tks is nothing but driver only,
+//		it is just having additional features of TakesScreenshot
+
+//		File src = tks.getScreenshotAs(OutputType.FILE);
+//		
+////		get the java representation object of the physical file for destination
+//		File dest = new File("./errorshots/webpage.png");
+////		File dest = new File("C:/Users/User/git/basic-sel-m10\\basic-selenium-m10\\errorshots\\insta.png");
+//		
+//		FileHandler.copy(src, dest);
+
+		Thread.sleep(3000);
+		WebElement loginBtn = driver.findElement(By.cssSelector("[aria-label='Log in']"));
+
+//		driver.getScreenshotAs();
+		File source = loginBtn.getScreenshotAs(OutputType.FILE);
+		File destination = new File("./errorshots/login.png");
+
+		FileHandler.copy(source, destination);
+
 		Thread.sleep(3000);
 		driver.quit();
 	}
